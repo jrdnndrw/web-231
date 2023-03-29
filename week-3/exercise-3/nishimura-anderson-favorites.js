82b9df5a646405d276f9ae54ebc754ca3e2244d3
Jordan Anderson <jordan.anderson8@icloud.com>
2023-03-29 11:19:57 -0500

    Adding the favorites app javascript functions

---
+++ week-3/exercise-3/nishimura-anderson-favorites.js
@@ -0,0 +1,37 @@
+/*
+============================================
+; Title: nishimura-anderson-favorites.js
+; Author: Jordan Nishimura-Anderson
+; Date: 3/28/2023
+; Description: This provides onclick event handling by generating
+; an alert for the user interface of the favorites app.
+;===========================================
+*/
+
+/**
+ * The favoriteSportsTeam function displays an alert box with a message.
+ */
+function favoriteSportsTeam () {
+  alert('My favorite sports team is the Minnesota Vikings!')
+}
+
+/**
+ * The favoriteBook function displays an alert box with a message.
+ */
+function favoriteBook () {
+  alert('My favorite book is Occupation: Skateboarder by Tony Hawk!')
+}
+
+/**
+ * The favoriteAuthor function displays an alert box with a message.
+ */
+function favoriteAuthor () {
+  alert('My favorite author is J. K. Rowling!')
+}
+
+/**
+ * The favoriteProgrammingLanguage function displays an alert box with a message.
+ */
+function favoriteProgrammingLanguage () {
+  alert('My favorite programming language is Javascript!')
+}
